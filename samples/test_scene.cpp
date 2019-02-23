897e78b30178abfb7340017e8d7deae0e5d7a96a
Magnus Bergsten <magbergs@student.chalmers.se>
2019-02-23 14:11:24 +0100

    Test scene: use pointer to scene to avoid static initialisation

--- samples/test_scene.cpp
+++ samples/test_scene.cpp
@@ -1,7 +1,5 @@
 #include "test_scene.h"
 
-#include <fmt/core.h>
-
 #include <mg/core/mg_config.h>
 #include <mg/core/mg_log.h>
 #include <mg/gfx/mg_material_repository.h>
@@ -11,12 +9,14 @@
 #include <mg/resources/mg_shader_resource.h>
 #include <mg/resources/mg_texture_resource.h>
 
+#include <fmt/core.h>
+
 using namespace Mg;
 using namespace Mg::gfx;
 using namespace Mg::input;
 
-// Global scene object. Since this is just a small sample, I feel okay with using a global.
-Scene g_scene;
+// Global scene pointer. Since this is just a small sample, I feel okay with using a global.
+Scene* g_scene;
 
 inline void setup_config()
 {
@@ -27,22 +27,22 @@ inline void setup_config()
 
 inline Mg::gfx::MeshHandle load_mesh(Identifier file)
 {
-    auto access = g_scene.resource_cache.access_resource<MeshResource>(file);
-    return g_scene.root.gfx_context().mesh_repository().create(*access);
+    auto access = g_scene->resource_cache.access_resource<MeshResource>(file);
+    return g_scene->root.gfx_context().mesh_repository().create(*access);
 }
 
 inline Mg::gfx::TextureHandle load_texture(std::string_view file)
 {
     auto file_name = Identifier::from_runtime_string(fmt::format("textures/{}.dds", file));
-    auto access    = g_scene.resource_cache.access_resource<TextureResource>(file_name);
-    return g_scene.root.gfx_context().texture_repository().create(*access);
+    auto access    = g_scene->resource_cache.access_resource<TextureResource>(file_name);
+    return g_scene->root.gfx_context().texture_repository().create(*access);
 }
 
 inline Mg::gfx::Material* load_material(Identifier file, std::initializer_list<Identifier> options)
 {
     auto handle =
-        g_scene.resource_cache.resource_handle<ShaderResource>("shaders/default.mgshader");
-    Material* m = g_scene.root.gfx_context().material_repository().create(file, handle);
+        g_scene->resource_cache.resource_handle<ShaderResource>("shaders/default.mgshader");
+    Material* m = g_scene->root.gfx_context().material_repository().create(file, handle);
 
     for (auto o : options) { m->set_option(o, true); }
 
@@ -119,12 +119,12 @@ void init()
 {
     setup_config();
 
-    auto& window = g_scene.root.window();
+    auto& window = g_scene->root.window();
 
     // Configure window
     {
         window.set_focus_callback([](bool is_focused) {
-            if (is_focused) { g_scene.resource_cache.refresh(); }
+            if (is_focused) { g_scene->resource_cache.refresh(); }
         });
 
         WindowSettings window_settings = read_display_settings(Root::config());
@@ -133,29 +133,29 @@ void init()
         window.set_cursor_lock_mode(CursorLockMode::LOCKED);
     }
 
-    g_scene.resource_cache.set_resource_reload_callback([](const FileChangedEvent& event) {
+    g_scene->resource_cache.set_resource_reload_callback([](const FileChangedEvent& event) {
         switch (event.resource.type_id().hash()) {
         case Identifier("TextureResource").hash():
-            g_scene.root.gfx_context().texture_repository().update(
+            g_scene->root.gfx_context().texture_repository().update(
                 static_cast<TextureResource&>(event.resource));
             break;
-        case Identifier("ShaderResource").hash(): g_scene.mesh_renderer.drop_shaders(); break;
+        case Identifier("ShaderResource").hash(): g_scene->mesh_renderer.drop_shaders(); break;
         default:
             g_log.write_verbose(fmt::format("Resource '{}' was updated, but ignored.",
                                             event.resource.resource_id().str_view()));
         }
     });
 
-    g_scene.hdr_target = make_hdr_target(window.settings().video_mode);
+    g_scene->hdr_target = make_hdr_target(window.settings().video_mode);
 
-    g_scene.root.gfx_context().set_clear_colour(0.0125f, 0.01275f, 0.025f);
+    g_scene->root.gfx_context().set_clear_colour(0.0125f, 0.01275f, 0.025f);
 
-    g_scene.camera.set_aspect_ratio(window.aspect_ratio());
-    g_scene.camera.field_of_view         = { FieldOfView::DEGREES, 80.0f };
-    g_scene.current_state.cam_position.z = 1.0f;
-    g_scene.prev_state                   = g_scene.current_state;
+    g_scene->camera.set_aspect_ratio(window.aspect_ratio());
+    g_scene->camera.field_of_view         = { FieldOfView::DEGREES, 80.0f };
+    g_scene->current_state.cam_position.z = 1.0f;
+    g_scene->prev_state                   = g_scene->current_state;
 
-    g_scene.input_map = make_input_map(window);
+    g_scene->input_map = make_input_map(window);
 
     // Load models
     {
@@ -167,18 +167,18 @@ void init()
         MaterialAssignment hest_mats[]    = { { 0, "actors/HestDraugr" } };
         MaterialAssignment narmask_mats[] = { { 0, "actors/narmask" } };
 
-        g_scene.scene_models.emplace_back(
+        g_scene->scene_models.emplace_back(
             load_model("meshes/misc/test_scene_2.mgm", scene_mats, { "PARALLAX" }));
 
         {
-            Model& hest_model = g_scene.scene_models.emplace_back(
+            Model& hest_model = g_scene->scene_models.emplace_back(
                 load_model("meshes/misc/hestdraugr.mgm", hest_mats, { "RIM_LIGHT" }));
 
             hest_model.transform.position.x = 3.0f;
         }
 
         {
-            Model& narmask_model = g_scene.scene_models.emplace_back(
+            Model& narmask_model = g_scene->scene_models.emplace_back(
                 load_model("meshes/misc/narmask.mgm", narmask_mats, { "RIM_LIGHT" }));
 
             narmask_model.transform.position.x -= 2.0f;
@@ -188,30 +188,30 @@ void init()
 
     // Create post-process material
     {
-        auto handle = g_scene.resource_cache.resource_handle<ShaderResource>(
+        auto handle = g_scene->resource_cache.resource_handle<ShaderResource>(
             "shaders/post_process_test.mgshader");
-        g_scene.post_material =
-            g_scene.root.gfx_context().material_repository().create("PostProcessMaterial", handle);
+        g_scene->post_material =
+            g_scene->root.gfx_context().material_repository().create("PostProcessMaterial", handle);
     }
 
-    g_scene.light_billboard_texture = load_texture("light_t");
+    g_scene->light_billboard_texture = load_texture("light_t");
 
     main_loop();
 }
 
 void time_step()
 {
-    auto& input      = g_scene.input_map;
-    auto& window     = g_scene.root.window();
-    auto& camera     = g_scene.camera;
-    auto& prev_state = g_scene.prev_state;
-    auto& state      = g_scene.current_state;
+    auto& input      = g_scene->input_map;
+    auto& window     = g_scene->root.window();
+    auto& camera     = g_scene->camera;
+    auto& prev_state = g_scene->prev_state;
+    auto& state      = g_scene->current_state;
     auto& config     = Root::config();
 
     window.poll_input_events();
     input.refresh();
 
-    if (input.was_pressed("exit")) { g_scene.exit = true; }
+    if (input.was_pressed("exit")) { g_scene->exit = true; }
 
     prev_state = state;
 
@@ -272,11 +272,11 @@ void time_step()
         s.video_mode     = {}; // reset resolution etc. to defaults
         window.apply_settings(s);
         camera.set_aspect_ratio(window.aspect_ratio());
-        g_scene.hdr_target = make_hdr_target(window.settings().video_mode);
+        g_scene->hdr_target = make_hdr_target(window.settings().video_mode);
         window.release_cursor();
     }
 
-    if (input.was_pressed("toggle_debug_vis")) { g_scene.draw_debug = !g_scene.draw_debug; }
+    if (input.was_pressed("toggle_debug_vis")) { g_scene->draw_debug = !g_scene->draw_debug; }
 }
 
 inline Scene::State lerp(const Scene::State& fst, const Scene::State& snd, double x)
@@ -295,23 +295,23 @@ inline void add_to_render_list(const Model& model, RenderCommandList& renderlist
 
 void render_scene(double lerp_factor)
 {
-    auto& gfx = g_scene.root.gfx_context();
+    auto& gfx = g_scene->root.gfx_context();
 
-    Scene::State render_state = lerp(g_scene.prev_state, g_scene.current_state, lerp_factor);
-    g_scene.camera.position   = render_state.cam_position;
-    g_scene.camera.rotation   = render_state.cam_rotation;
+    Scene::State render_state = lerp(g_scene->prev_state, g_scene->current_state, lerp_factor);
+    g_scene->camera.position  = render_state.cam_position;
+    g_scene->camera.rotation  = render_state.cam_rotation;
 
     // Draw meshes
-    RenderCommandList& render_list = g_scene.render_list;
+    RenderCommandList& render_list = g_scene->render_list;
     render_list.clear();
-    for (auto&& model : g_scene.scene_models) { add_to_render_list(model, render_list); }
+    for (auto&& model : g_scene->scene_models) { add_to_render_list(model, render_list); }
 
     std::vector<Light> lights;
 
     // Create a lot of random lights
     // I know rand / srand is not much good but this is just a little sample...
     std::srand(222);
-    g_scene.billboard_render_list.clear();
+    g_scene->billboard_render_list.clear();
     for (size_t i = 0; i < k_num_lights; ++i) {
         auto pos = glm::vec3(rand(), rand(), 0.0f);
         pos /= RAND_MAX;
@@ -323,12 +323,12 @@ void render_scene(double lerp_factor)
         colour /= RAND_MAX;
 
         float offset = float(rand()) / RAND_MAX * 7.0f;
-        float s      = float(sin(g_scene.time * 0.5 + offset));
+        float s      = float(sin(g_scene->time * 0.5 + offset));
 
         pos.z += s;
 
         {
-            Billboard& billboard = g_scene.billboard_render_list.add();
+            Billboard& billboard = g_scene->billboard_render_list.add();
             billboard.pos        = pos;
             billboard.colour     = colour;
             billboard.radius     = 0.1f;
@@ -337,29 +337,29 @@ void render_scene(double lerp_factor)
         lights.push_back(make_point_light(pos, colour * 100.0f, k_light_radius));
     }
 
-    render_list.frustum_cull_draw_list(g_scene.camera);
-    render_list.sort_draw_list(g_scene.camera, SortFunc::NEAR_TO_FAR);
+    render_list.frustum_cull_draw_list(g_scene->camera);
+    render_list.sort_draw_list(g_scene->camera, SortFunc::NEAR_TO_FAR);
 
-    g_scene.hdr_target->bind();
+    g_scene->hdr_target->bind();
     gfx.clear();
 
-    g_scene.mesh_renderer.render(g_scene.camera, render_list, lights);
+    g_scene->mesh_renderer.render(g_scene->camera, render_list, lights);
 
-    g_scene.billboard_renderer.render(g_scene.camera,
-                                      g_scene.billboard_render_list,
-                                      g_scene.light_billboard_texture,
-                                      BillboardSetting::A_TEST);
+    g_scene->billboard_renderer.render(g_scene->camera,
+                                       g_scene->billboard_render_list,
+                                       g_scene->light_billboard_texture,
+                                       BillboardSetting::A_TEST);
 
-    g_scene.root.window().render_target.bind();
+    g_scene->root.window().render_target.bind();
     gfx.clear();
 
-    g_scene.post_renderer.post_process(*g_scene.post_material,
-                                       g_scene.hdr_target->colour_target(),
-                                       g_scene.hdr_target->depth_target(),
-                                       g_scene.camera.depth_range().near(),
-                                       g_scene.camera.depth_range().far());
+    g_scene->post_renderer.post_process(*g_scene->post_material,
+                                        g_scene->hdr_target->colour_target(),
+                                        g_scene->hdr_target->depth_target(),
+                                        g_scene->camera.depth_range().near(),
+                                        g_scene->camera.depth_range().far());
 
-    if (g_scene.draw_debug) {
+    if (g_scene->draw_debug) {
         gfx.set_depth_test(DepthFunc::NONE);
 
         for (const Light& light : lights) {
@@ -369,34 +369,34 @@ void render_scene(double lerp_factor)
             params.colour     = glm::vec4(normalize(light.colour), 0.5f);
             params.dimensions = glm::vec3(std::sqrt(light.range_sqr));
             params.wireframe  = true;
-            g_scene.debug_renderer.draw_ellipsoid(g_scene.camera, params);
+            g_scene->debug_renderer.draw_ellipsoid(g_scene->camera, params);
         }
 
         gfx.set_depth_test(DepthFunc::LESS);
     }
 
-    g_scene.root.window().refresh();
+    g_scene->root.window().refresh();
 }
 
 void main_loop()
 {
     double accumulator    = 0.0;
-    double last_fps_write = g_scene.time;
+    double last_fps_write = g_scene->time;
     size_t n_frames       = 0;
 
-    while (!g_scene.exit) {
-        double last_time = g_scene.time;
-        g_scene.time     = g_scene.root.time_since_init();
+    while (!g_scene->exit) {
+        double last_time = g_scene->time;
+        g_scene->time    = g_scene->root.time_since_init();
 
         ++n_frames;
 
-        if (g_scene.time - last_fps_write > 1.0) {
+        if (g_scene->time - last_fps_write > 1.0) {
             g_log.write_verbose(fmt::format("{} FPS", n_frames));
-            last_fps_write = g_scene.time;
+            last_fps_write = g_scene->time;
             n_frames       = 0;
         }
 
-        accumulator += g_scene.time - last_time;
+        accumulator += g_scene->time - last_time;
         accumulator = std::min(accumulator, k_accumulator_max_steps * k_time_step);
 
         while (accumulator >= k_time_step) {
@@ -410,6 +410,8 @@ void main_loop()
 
 int main(int /*argc*/, char* /*argv*/ [])
 {
+    Scene scene;
+    g_scene = &scene;
     init();
 }
 
