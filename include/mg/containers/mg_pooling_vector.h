99089e744db83192f9432ab29c689735a145c34d
Magnus Bergsten <magbergs@student.chalmers.se>
2019-02-23 21:36:18 +0100

    Refactor PoolingVector: now more efficient and self-contained
    Less storage overhead and has fewer standard-library dependencies.

--- include/mg/containers/mg_pooling_vector.h
+++ include/mg/containers/mg_pooling_vector.h
@@ -28,15 +28,68 @@
 
 #pragma once
 
-#include <cstddef>
-#include <memory>
-#include <optional>
-#include <vector>
-
+#include "mg/containers/mg_array.h"
 #include "mg/containers/mg_small_vector.h"
 #include "mg/utils/mg_assert.h"
+#include "mg/utils/mg_macros.h"
+
+#include <cstddef>
+#include <cstdint>
 
 namespace Mg {
+
+namespace detail {
+
+// A pool for storage of objects in PoolingVector.
+// PoolingVector allocates a new one of these whenever the existing ones are full.
+template<typename T> class StoragePool {
+public:
+    MG_MAKE_DEFAULT_COPYABLE(StoragePool);
+    MG_MAKE_DEFAULT_MOVABLE(StoragePool);
+
+    StoragePool() = default;
+
+    StoragePool(size_t size)
+        : m_storage(Array<StorageT>::make(size)), m_present(Array<bool>::make(size))
+    {}
+
+    ~StoragePool()
+    {
+        for (size_t i = 0; i < m_storage.size(); ++i) {
+            if (m_present[i]) { destroy(i); }
+        }
+    }
+
+    template<typename... Args> T& emplace(size_t i, Args&&... args)
+    {
+        if (m_present[i]) { destroy(i); }
+
+        T* ptr       = new (&m_storage[i]) T(std::forward<Args>(args)...);
+        m_present[i] = true;
+
+        return *ptr;
+    }
+
+    T& get(size_t i)
+    {
+        MG_ASSERT_DEBUG(m_present[i]);
+        return reinterpret_cast<T&>(m_storage[i]); // NOLINT
+    }
+
+    void destroy(size_t i)
+    {
+        MG_ASSERT_DEBUG(m_present[i]);
+        get(i).~T();
+        m_present[i] = false;
+    }
+
+private:
+    using StorageT = std::aligned_storage_t<sizeof(T), alignof(T)>;
+    Array<StorageT> m_storage;
+    Array<bool>     m_present; // TODO: this should ideally be a bit-field instead of array of bool.
+};
+
+} // namespace detail
 
 /** Dynamic-array data structure which grows by allocating a fixed-size 'pool' whenever it is out of
  * space. Hence, elements never get moved, and pointers/references to elements remain valid until
@@ -46,7 +99,16 @@ namespace Mg {
  */
 template<typename T> class PoolingVector {
 public:
-    explicit PoolingVector(size_t pool_size) : m_pool_size{ pool_size } { _grow(); }
+    /** Construct.
+     * @param pool_size Size of each individual element-pool (in number of elements). The
+     * PoolingVector will allocate storage for elements in pools of this size.
+     * Must be greater than zero.
+     */
+    explicit PoolingVector(size_t pool_size) : m_pool_size{ pool_size }
+    {
+        MG_ASSERT(pool_size > 0);
+        _grow();
+    }
 
     struct ConstructReturn {
         uint32_t index;
@@ -54,7 +116,7 @@ public:
     };
 
     /** Construct object using supplied arguments.
-     * Time complexity: amortised constant time (may allocate if full).
+     * Time complexity: constant time (but may allocate a new pool if full).
      * @return struct of `uint32_t index; T* ptr;`
      */
     template<typename... Args> ConstructReturn construct(Args&&... args)
@@ -65,7 +127,7 @@ public:
         m_free_indices.pop_back();
 
         const ElemIndex ei{ _internal_index(index) };
-        T* ptr = &m_pools[ei.pool_index][ei.element_index].emplace(std::forward<Args>(args)...);
+        T* ptr = &m_pools[ei.pool_index].emplace(ei.element_index, std::forward<Args>(args)...);
 
         return { index, ptr };
     }
@@ -78,21 +140,19 @@ public:
     {
         const ElemIndex ei{ _internal_index(index) };
         m_free_indices.push_back(index);
-        m_pools[ei.pool_index][ei.element_index] = std::nullopt;
+        m_pools[ei.pool_index].destroy(ei.element_index);
     }
 
     T& operator[](uint32_t index) noexcept
     {
         const ElemIndex ei{ _internal_index(index) };
-        MG_ASSERT_DEBUG(m_pools[ei.pool_index][ei.element_index].has_value());
-        return *m_pools[ei.pool_index][ei.element_index];
+        return m_pools[ei.pool_index].get(ei.element_index);
     }
 
     const T& operator[](uint32_t index) const noexcept
     {
         const ElemIndex ei{ _internal_index(index) };
-        MG_ASSERT_DEBUG(m_pools[ei.pool_index][ei.element_index].has_value());
-        return *m_pools[ei.pool_index][ei.element_index];
+        return m_pools[ei.pool_index].get(ei.element_index);
     }
 
 private:
@@ -101,6 +161,7 @@ private:
         size_t element_index{};
     };
 
+    // Index within the respective pool.
     ElemIndex _internal_index(size_t index) const noexcept
     {
         const auto pool_index    = index / m_pool_size;
@@ -116,7 +177,7 @@ private:
     {
         MG_ASSERT(m_pool_size > 0);
 
-        m_pools.emplace_back(std::make_unique<Pool>(m_pool_size));
+        m_pools.emplace_back(m_pool_size);
 
         const size_t new_index_start = (m_pools.size() - 1) * m_pool_size;
         const size_t new_index_end   = new_index_start + m_pool_size;
@@ -127,13 +188,10 @@ private:
         }
     }
 
-    using StorageT = std::optional<T>;
-    using Pool     = StorageT[];
-
     size_t m_pool_size{};
 
-    small_vector<std::unique_ptr<Pool>, 10> m_pools;
-    std::vector<uint32_t>                   m_free_indices;
+    small_vector<detail::StoragePool<T>, 1> m_pools;
+    small_vector<uint32_t, 1>               m_free_indices;
 };
 
 } // namespace Mg
