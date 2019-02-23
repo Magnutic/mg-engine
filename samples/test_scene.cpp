1161e5a2af7372bac9670aa19b9b6fa8fe6f30a1
Magnus Bergsten <magbergs@student.chalmers.se>
2019-02-23 16:54:28 +0100

    Minor formatting

--- samples/test_scene.cpp
+++ samples/test_scene.cpp
@@ -139,7 +139,11 @@ void init()
             g_scene->root.gfx_device().texture_repository().update(
                 static_cast<TextureResource&>(event.resource));
             break;
-        case Identifier("ShaderResource").hash(): g_scene->mesh_renderer.drop_shaders(); break;
+
+        case Identifier("ShaderResource").hash():
+            g_scene->mesh_renderer.drop_shaders();
+            break;
+
         default:
             g_log.write_verbose(fmt::format("Resource '{}' was updated, but ignored.",
                                             event.resource.resource_id().str_view()));
