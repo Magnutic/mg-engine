977cb4ed3e6dd5960fb74c707b16081c68d88474
Magnus Bergsten <magbergs@student.chalmers.se>
2019-03-17 23:15:09 +0100

    MeshRenderer: Move pipeline factory setup into same file.

--- src/gfx/opengl/mg_mesh_renderer.cpp
+++ src/gfx/opengl/mg_mesh_renderer.cpp
@@ -23,22 +23,127 @@
 
 #include "mg/gfx/mg_mesh_renderer.h"
 
-#include "mg_render_command_data.h"
-#include "shader_factories/mg_mesh_shader_provider.h"
-
 #include "mg_gl_debug.h"
+#include "mg_render_command_data.h"
+#include "shader_factories/shader_code/mg_mesh_framework_shader_code.h"
 #include "mg_glad.h"
 
+#include "mg/gfx/mg_camera.h"
 #include "mg/gfx/mg_light_buffers.h"
 #include "mg/gfx/mg_light_grid.h"
 #include "mg/gfx/mg_material.h"
 #include "mg/gfx/mg_matrix_ubo.h"
+#include "mg/gfx/mg_pipeline_repository.h"
 #include "mg/gfx/mg_render_command_list.h"
 #include "mg/gfx/mg_vertex.h"
 
 #include "mg_texture_node.h"
 
 namespace Mg::gfx {
+
+namespace { // internal linkage
+
+constexpr uint32_t k_matrix_ubo_slot          = 0;
+constexpr uint32_t k_frame_ubo_slot           = 1;
+constexpr uint32_t k_light_ubo_slot           = 2;
+constexpr uint32_t k_material_params_ubo_slot = 3;
+
+constexpr uint32_t k_sampler_tile_data_index   = 8;
+constexpr uint32_t k_sampler_light_index_index = 9; // Index of sampler for light indices
+
+/** Location of '_matrix_index' vertex attribute in shader code. */
+constexpr uint32_t k_matrix_index_vertex_attrib_location = 8;
+
+// Parameters used to calculate cluster slice from fragment depth
+struct ClusterGridParams {
+    glm::vec2 z_param;
+    float     scale = 0.0f;
+    float     bias  = 0.0f;
+};
+
+/** Frame-global UBO block */
+struct FrameBlock {
+    ClusterGridParams cluster_grid_params;
+
+    // .xyz: camera_position; .w: time. vec4 for alignment purposes.
+    glm::vec4 camera_position_and_time;
+
+    glm::uvec2 viewport_size;
+
+    float camera_exposure = 0.0f;
+};
+
+constexpr const char* mesh_fs_fallback = R"(
+float attenuate(float distance_sqr, float range_sqr_reciprocal) { return 1.0; }
+
+vec3 light(const LightInput light, const SurfaceParams surface, const vec3 view_direction) {
+    return vec3(0.0);
+}
+
+void final_colour(const SurfaceInput s_in, const SurfaceParams s, inout vec4 colour) {}
+
+void surface(const SurfaceInput s_in, out SurfaceParams s_out) {
+    s_out.albedo    = vec3(0.0);
+    s_out.specular  = vec3(0.0);
+    s_out.gloss     = 0.0;
+    s_out.normal    = vec3(0.0);
+    s_out.emission  = vec3(100.0, 0.0, 100.0);
+    s_out.occlusion = 0.0;
+    s_out.alpha     = 1.0;
+}
+)";
+
+FrameBlock make_frame_block(const ICamera& camera, float current_time, float camera_exposure)
+{
+    std::array<int, 4> viewport_data{};
+    glGetIntegerv(GL_VIEWPORT, &viewport_data[0]);
+    glm::uvec2 viewport_size{ narrow<uint32_t>(viewport_data[2]),
+                              narrow<uint32_t>(viewport_data[3]) };
+
+    static const float scale = MG_LIGHT_GRID_DEPTH / std::log2(float(MG_LIGHT_GRID_FAR_PLANE));
+
+    const auto depth_range = camera.depth_range();
+    const auto z_near      = depth_range.near();
+    const auto z_far       = depth_range.far();
+    const auto c           = std::log2(2.0f * z_far * z_near);
+
+    FrameBlock frame_block;
+    frame_block.camera_position_and_time = glm::vec4(camera.get_position(), current_time);
+    frame_block.viewport_size            = viewport_size;
+
+    frame_block.cluster_grid_params.z_param = glm::vec2(z_near - z_far, z_near + z_far);
+    frame_block.cluster_grid_params.scale   = -scale;
+    frame_block.cluster_grid_params.bias    = float(MG_LIGHT_GRID_DEPTH_BIAS) + c * scale;
+
+    frame_block.camera_exposure = camera_exposure;
+
+    return frame_block;
+}
+
+experimental::PipelineRepository make_mesh_pipeline_repository()
+{
+    using namespace experimental;
+
+    PipelineRepository::Config config{};
+    config.preamble_shader_code = { VertexShaderCode{ k_lit_mesh_framework_vertex_code },
+                                    {},
+                                    FragmentShaderCode{ k_lit_mesh_framework_fragment_code } };
+
+    config.on_error_shader_code = { {}, {}, FragmentShaderCode{ mesh_fs_fallback } };
+
+    config.pipeline_prototype.common_input_layout = {
+        { "MatrixBlock", PipelineInputType::UniformBuffer, k_matrix_ubo_slot },
+        { "FrameBlock", PipelineInputType::UniformBuffer, k_frame_ubo_slot },
+        { "LightBlock", PipelineInputType::UniformBuffer, k_light_ubo_slot },
+        { "MaterialParams", PipelineInputType::UniformBuffer, k_material_params_ubo_slot },
+        { "_sampler_tile_data", PipelineInputType::BufferTexture, k_sampler_tile_data_index },
+        { "_sampler_light_index", PipelineInputType::BufferTexture, k_sampler_light_index_index }
+    };
+
+    return PipelineRepository(config);
+}
+
+} // namespace
 
 /** MeshRenderer's state. */
 struct MeshRendererData {
@@ -47,7 +152,7 @@ struct MeshRendererData {
     MatrixUniformHandler m_matrix_uniform_handler;
 
     // Frame-global uniform buffer
-    UniformBuffer m_frame_ubo{ sizeof(mesh_renderer::FrameBlock) };
+    UniformBuffer m_frame_ubo{ sizeof(FrameBlock) };
 
     // Material parameters uniform buffer
     UniformBuffer m_material_params_ubo{ defs::k_material_parameters_buffer_size };
@@ -61,10 +166,12 @@ struct MeshRendererData {
     Pipeline* m_current_pipeline{};
 };
 
+namespace {
+
 /** Set current pipeline to the one required for the given material. */
-inline void bind_pipeline(MeshRendererData&         data,
-                          const Material&           material,
-                          PipelinePrototypeContext& pipeline_prototype_context)
+void bind_pipeline(MeshRendererData&         data,
+                   const Material&           material,
+                   PipelinePrototypeContext& pipeline_prototype_context)
 {
     const auto new_shader_hash = material.shader_hash();
 
@@ -77,12 +184,10 @@ inline void bind_pipeline(MeshRendererData&         data,
 }
 
 /** Set pipeline input to match the given material. */
-inline void set_material(MeshRendererData&         data,
-                         const Material&           material,
-                         PipelinePrototypeContext& pipeline_prototype_context)
+void set_material(MeshRendererData&         data,
+                  const Material&           material,
+                  PipelinePrototypeContext& pipeline_prototype_context)
 {
-    using namespace mesh_renderer;
-
     bind_pipeline(data, material, pipeline_prototype_context);
     MG_ASSERT(data.m_current_pipeline != nullptr);
 
@@ -99,15 +204,12 @@ inline void set_material(MeshRendererData&         data,
 }
 
 /** Upload frame-constant buffers to GPU. */
-inline void
-upload_frame_constant_buffers(MeshRendererData& data, const ICamera& cam, RenderParameters params)
+void upload_frame_constant_buffers(MeshRendererData& data,
+                                   const ICamera&    cam,
+                                   RenderParameters  params)
 {
-    using namespace mesh_renderer;
-
     // Upload frame-global uniforms
-    FrameBlock frame_block = mesh_renderer::make_frame_block(cam,
-                                                             params.current_time,
-                                                             params.camera_exposure);
+    FrameBlock frame_block = make_frame_block(cam, params.current_time, params.camera_exposure);
     data.m_frame_ubo.set_data(byte_representation(frame_block));
 
     std::array input_bindings =
@@ -125,16 +227,18 @@ upload_frame_constant_buffers(MeshRendererData& data, const ICamera& cam, Render
     // shader has been changed in between invocations of this renderer's loop.
 }
 
-inline void draw_elements(size_t num_elements, size_t starting_element)
+void draw_elements(size_t num_elements, size_t starting_element)
 {
     auto begin = reinterpret_cast<GLvoid*>(starting_element * sizeof(uint_vertex_index)); // NOLINT
     glDrawElements(GL_TRIANGLES, int32_t(num_elements), GL_UNSIGNED_SHORT, begin);
 }
 
-inline void set_matrix_index(uint32_t index)
+void set_matrix_index(uint32_t index)
 {
-    glVertexAttribI1ui(mesh_renderer::k_matrix_index_vertex_attrib_location, index);
+    glVertexAttribI1ui(k_matrix_index_vertex_attrib_location, index);
 }
+
+} // namespace
 
 //--------------------------------------------------------------------------------------------------
 // MeshRenderer implementation
@@ -155,7 +259,6 @@ void MeshRenderer::render(const ICamera&           cam,
                           RenderParameters         params)
 {
     using namespace internal;
-    using namespace mesh_renderer;
 
     auto            current_vao      = uint32_t(-1);
     const Material* current_material = nullptr;
