f20f9aa7e70e6a4d5764e056bfd616ce54e37a27
Magnus Bergsten <magnus@magnutic.se>
2020-03-27 22:35:31 +0100

    Use WIN32 API to read file time stamps on Windows.
    This solution also works under MinGW.

--- src/utils/mg_file_time_helper.cpp
+++ src/utils/mg_file_time_helper.cpp
@@ -13,30 +13,41 @@
 #include "mg/core/mg_log.h"
 #include "mg/core/mg_runtime_error.h"
 
-#ifdef _MSC_VER
+#include <fmt/core.h>
+
+#ifdef _WIN32
 #    define WIN32_LEAN_AND_MEAN
 #    include "Windows.h"
-#endif // _MSC_VER
+#endif // _WIN32
 
 namespace Mg {
 
-// TODO: implement portably (probably only possible with C++20)
+// TODO C++20: Replace with using std::filesystem::last_write_time and std::chrono::clock_cast.
 
 // Get last write time of file as time_t (more portable than the unspecified time type guaranteed by
 // C++17 standard).
 std::time_t last_write_time_t(const std::filesystem::path& file)
 {
-    const auto filetime = std::filesystem::last_write_time(file);
-
-#ifdef _MSC_VER
-    // For MSVC, manually convert to time_t.
+#ifdef _WIN32
+    // For Windows, manually convert to time_t.
+    HANDLE handle = CreateFileW(file.wstring().c_str(),
+                                GENERIC_READ,
+                                FILE_SHARE_READ,
+                                nullptr,
+                                OPEN_EXISTING,
+                                FILE_ATTRIBUTE_NORMAL,
+                                nullptr);
     FILETIME ft;
+    const bool gotTime = GetFileTime(handle, nullptr, nullptr, &ft);
+
+    if (!gotTime) {
+        const auto msg = fmt::format("Could not read time stamp of file '{}'",
+                                     file.generic_u8string());
+        g_log.write_error(msg);
+        throw RuntimeError{};
+    }
+
     SYSTEMTIME st;
-
-    // filesystem time stamps are supposed to be binary compatible with Windows FILETIME objects in
-    // MSVC stdlib.
-    memcpy(&ft, &filetime, sizeof(FILETIME));
-
     if (FileTimeToSystemTime(&ft, &st) == 0) {
         g_log.write_error("Failed to convert file time stamp to 'time_t'.");
         throw RuntimeError{};
@@ -54,8 +65,9 @@ std::time_t last_write_time_t(const std::filesystem::path& file)
     return std::mktime(&tm);
 #else
     // Not guaranteed to be portable, but works with libstdc++ (GCC).
+    const auto filetime = std::filesystem::last_write_time(file);
     return std::chrono::system_clock::to_time_t(filetime);
-#endif // _MSC_VER
+#endif // _WIN32
 }
 
 } // namespace Mg
