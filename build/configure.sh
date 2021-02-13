812f4b51dfcdfe6867cb7dbfd6798031b9308dfd
Magnus Bergsten <magbergs@student.chalmers.se>
2021-02-13 19:54:41 +0100

    Improve build-helper script

--- build/configure.sh
+++ build/configure.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname $(realpath "${BASH_SOURCE:-0}"))
+SCRIPT_NAME=$(basename "${BASH_SOURCE:-0}")
 BUILD_ROOT=$SCRIPT_DIR/out
 SRC_ROOT=$SCRIPT_DIR/..
+
+echo "---- $SCRIPT_NAME ----"
 
 if [ ! -d $BUILD_ROOT/deps-install ]; then
     echo "Dependencies not installed to $BUILD_ROOT/deps-install. Run build-dependencies.sh first."
@@ -10,40 +13,68 @@ if [ ! -d $BUILD_ROOT/deps-install ]; then
 fi
 
 if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 configuration-name c-compiler cxx-compiler [flags]"
-    echo "Flags: --debug: debug build (default is release), --use-sanitisers: enable runtime debug sanitisers, --use-gfx-debug-groups: enable debug groups (for use with e.g. apitrace)"
+    echo "Usage: $0 configuration-name c-compiler cxx-compiler [args]"
+    echo "Arguments:"
+    echo "    --debug Debug build (default is release)"
+    echo "    --use-sanitisers Enable runtime debug sanitisers"
+    echo "    --use-gfx-debug-groups Enable debug groups (for use with e.g. apitrace)"
+    echo "    --clang-tidy-command <command> Run clang-tidy when building using the given command."
     echo "Example: $0 clang-deb clang-8 clang++-8 --debug --use-sanitisers --use-gfx-debug-groups"
     echo "Note: this script can be invoked multiple times to set up different configurations, as long as the configuration-names differ."
     exit
 fi
 
-if [[ $* == *--use-sanitisers* ]]; then
-    USE_SANITISERS=1
-else
-    USE_SANITISERS=0
-fi
-
-if [[ $* == *--use-gfx-debug-groups* ]]; then
-    USE_GFX_DEBUG_GROUPS=1
-else
-    USE_GFX_DEBUG_GROUPS=0
-fi
-
-if [[ $* == *--debug* ]]; then
-    BUILD_TYPE=Debug
-else
-    BUILD_TYPE=Release
-fi
-
 CONFNAME=$1
 C_COMPILER=$2
 CXX_COMPILER=$3
+shift 3
 
-echo "Configuring '$CONFNAME' with C compiler '$C_COMPILER' and C++ compiler '$CXX_COMPILER'. Build type: $BUILD_TYPE. Sanitisers enabled: $USE_SANITISERS. Use gfx debug groups: $USE_GFX_DEBUG_GROUPS"
+BUILD_TYPE=Release
+USE_SANITISERS=0
+USE_GFX_DEBUG_GROUPS=0
+CLANG_TIDY_COMMAND=""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        --debug)
+        BUILD_TYPE=Debug
+        shift
+        ;;
+        --use-sanitisers|--use-sanitizers)
+        USE_SANITISERS=1
+        shift
+        ;;
+        --use-gfx-debug-groups)
+        USE_GFX_DEBUG_GROUPS=1
+        shift
+        ;;
+        --clang-tidy-command)
+        CLANG_TIDY_COMMAND="$2"
+        shift
+        shift
+        ;;
+        *)
+        echo "Unknown option: $1" >&2
+        exit 1
+        ;;
+    esac
+done
+
+echo "Configuring '$CONFNAME' with C compiler '$C_COMPILER' and C++ compiler '$CXX_COMPILER'."
+echo "Build type: $BUILD_TYPE."
+echo "Sanitisers enabled: $USE_SANITISERS."
+echo "Use gfx debug groups: $USE_GFX_DEBUG_GROUPS."
+if [[ $CLANG_TIDY_COMMAND != "" ]]; then
+    echo "Clang-tidy enabled with command: $CLANG_TIDY_COMMAND"
+fi
 
 rm -rf $BUILD_ROOT/$CONFNAME
 mkdir $BUILD_ROOT/$CONFNAME
 (cd $BUILD_ROOT/$CONFNAME &&
 cmake $SRC_ROOT -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_PREFIX_PATH=$BUILD_ROOT/deps-install \
     -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER -DMG_DEBUG_SANITISERS=$USE_SANITISERS \
-    -DMG_ENABLE_GFX_DEBUG_GROUPS=$USE_GFX_DEBUG_GROUPS)
+    -DMG_ENABLE_GFX_DEBUG_GROUPS=$USE_GFX_DEBUG_GROUPS \
+    -DCMAKE_CXX_CLANG_TIDY=$CLANG_TIDY_COMMAND)
