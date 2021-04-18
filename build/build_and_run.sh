47242f29ae3eedf10e0c6cf87c26cb63240f9654
Magnus Bergsten <magnus@magnutic.se>
2021-04-18 22:58:31 +0200

    Helper build script build_and_run.sh: allow building only desired target

--- build/build_and_run.sh
+++ build/build_and_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd "${BASH_SOURCE%/*}/" || exit 1
 
-./build.sh $1
-[[ $? -eq 0 ]] || exit 1
-
+BUILD_CONFIG=$1
 BUILD_TARGET="test_scene"
 [[ $# -eq 2 ]] && BUILD_TARGET=$2
+
+./build.sh $BUILD_CONFIG $BUILD_TARGET
+[[ $? -eq 0 ]] || exit 1
 
 ./out/$1/bin/${BUILD_TARGET}
