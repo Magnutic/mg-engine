9b1ae280731e89bd5366af9ecea7dbc9c35110df
Magnus Bergsten <magnus@magnutic.se>
2024-04-28 17:59:28 +0200

    Remove empty files

--- src/input/mg_joy.cpp
+++
@@ -1,7 +0,0 @@
-//**************************************************************************************************
-// This file is part of Mg Engine. Copyright (c) 2020, Magnus Bergsten.
-// Mg Engine is made available under the terms of the 3-Clause BSD License.
-// See LICENSE.txt in the project's root directory.
-//**************************************************************************************************
-
-#include "mg/input/mg_joy.h"
