37d53e02da1b0d35728a38e7b4e3e7a8b0d0d895
Magnus Bergsten <magnus@magnutic.se>
2025-05-25 15:08:14 +0200

    run_tests.sh: forward args

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "${BASH_SOURCE%/*}/" || exit 1
 
-./build.sh $*
+./build.sh $1
 [[ $? -eq 0 ]] || exit 1
-
 BUILD_DIR="out/$1"
-(cd $BUILD_DIR && CTEST_OUTPUT_ON_FAILURE=1 ctest) || exit 1
+shift
+(cd $BUILD_DIR && CTEST_OUTPUT_ON_FAILURE=1 ctest $@) || exit 1
